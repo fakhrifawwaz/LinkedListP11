08518782b8b75ab2f597be881f89d7f38b59f7a3
fakhrifawwaz <fakhri07fawwaz@gmail.com>
2026-05-06 10:49:24 +0700

    mainsl-2

--- SLLMain15.java
+++ SLLMain15.java
@@ -8,14 +8,32 @@ public class SLLMain15 {
         Mahasiswa15 mhs3 = new Mahasiswa15("21212202", "Cintia", "3C", 3.5);
         Mahasiswa15 mhs4 = new Mahasiswa15("21212203", "Dirga", "4D", 3.6);
 
+    // latihan 1
+    sll.print();
+ 
+    sll.addFirst(mhs1);
+    sll.print();
+ 
+    sll.addLast(mhs4);
+    sll.print();
+        
+    sll.insertAfter("Dirga", mhs3);
+    sll.insertAt(2, mhs2);
+    sll.print();
 
-        sll.print();
-        sll.addFirst(mhs4);
-        sll.print();
-        sll.addLast(mhs1);
-        sll.print();
-        sll.insertAfter("Dirga", mhs3);
-        sll.insertAt(2, mhs2);
-        sll.print();
+    System.out.print("data index 1 : ");
+    sll.getData(1);
+
+    // latihan 2
+    System.out.println("data mahasiswa an Bimon berada pada index : " + sll.indexOf("Bimon"));
+    System.out.println();
+
+    sll.removeFirst();
+    sll.removeLast();
+    sll.print();
+
+    sll.removeAt(0);
+    sll.print();
+
     }
 }
